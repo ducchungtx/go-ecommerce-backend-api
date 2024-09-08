d6c48c6a474ea218d1fc549336af9231aa0e5ffb
Chung Nguyen <zrmedia9@gmail.com>
2024-09-08 08:57:09 +0700

    101: gin vs viper (basic) feat: Add Viper configuration for server and database
    This commit adds a new file `main.viper.go` under `cmd/cli/viper` directory to implement Viper configuration for the server and database. It also includes a new configuration file `local.yaml` under `config` directory with server, database, and security settings.
    
    The `main.viper.go` file sets up Viper to read the `local.yaml` configuration file and retrieves the server port and JWT key values. This allows for easy configuration of the server and database settings.
    
    This commit enhances the project by introducing a centralized configuration management using Viper, making it easier to manage and modify the server and database settings.

---
+++ cmd/cli/viper/main.viper.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper := viper.New()
+	viper.AddConfigPath("./config/") // config file path
+	viper.SetConfigName("local")     // config file name without extension
+	viper.SetConfigType("yaml")      // config file type
+
+	// Read config file
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("Failed to read config file: " + err.Error()) // handle errors reading the config file and stop the program
+	}
+
+	// read config
+	// server port
+	fmt.Println("server.port:", viper.GetInt("server.port")) // server.port: 8080
+	// read jwt key
+	fmt.Println("jwt.key:", viper.GetString("security.jwt.key")) // jwt.key: secret
+}
