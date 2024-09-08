e8d5e1556dd695584d9aa4e708b23760f2729c89
Chung Nguyen <zrmedia9@gmail.com>
2024-09-08 11:48:40 +0700

    feat: Add middleware for logging before and after each request

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,13 +1,38 @@
 package routers
 
 import (
+	"fmt"
 	c "goecommerce/internal/controller"
 
 	"github.com/gin-gonic/gin"
 )
 
+func AA() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("Before --> AA")
+		c.Next()
+		fmt.Println("After --> AA")
+	}
+}
+
+func BB() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("Before --> BB")
+		c.Next()
+		fmt.Println("After --> BB")
+	}
+}
+
+func CC(c *gin.Context) {
+	fmt.Println("Before --> CC")
+	c.Next()
+	fmt.Println("After --> CC")
+}
+
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(AA(), BB(), CC)
+
 	v1 := r.Group("/v1/2024")
 	{
 		v1.GET("/ping", c.NewPongController().Pong)
