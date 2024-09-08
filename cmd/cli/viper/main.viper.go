48eaf6232a3b1d56488de1646f526edb9a77f01f
Chung Nguyen <zrmedia9@gmail.com>
2024-09-08 09:07:17 +0700

    101: gin vs viper feat: Update main.viper.go to read and print database configurations
    This commit updates the main.viper.go file to read the database configurations from the local.yaml file and print them. It adds a new struct, Config, to represent the configuration and uses viper.Unmarshal to read the configurations into the struct. The updated code then prints the server port and the details of each database.

--- cmd/cli/viper/main.viper.go
+++ cmd/cli/viper/main.viper.go
@@ -6,6 +6,17 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Config struct {
+	Server struct {
+		Port int `mapstructure:"port"`
+	} `mapstructure:"server"`
+	Databases []struct {
+		User     string `mapstructure:"user"`
+		Password string `mapstructure:"password"`
+		Host     string `mapstructure:"host"`
+	} `mapstructure:"databases"`
+}
+
 func main() {
 	viper := viper.New()
 	viper.AddConfigPath("./config/") // config file path
@@ -23,4 +34,19 @@ func main() {
 	fmt.Println("server.port:", viper.GetInt("server.port")) // server.port: 8080
 	// read jwt key
 	fmt.Println("jwt.key:", viper.GetString("security.jwt.key")) // jwt.key: secret
+
+	// read databases
+	var config Config
+	// read config into struct
+	if err := viper.Unmarshal(&config); err != nil {
+		panic("Failed to unmarshal config: " + err.Error())
+	}
+
+	// print config
+	fmt.Println("server.port:", config.Server.Port) // server.port: 8080
+
+	for _, db := range config.Databases {
+		// fmt.Println("database:", db)
+		fmt.Printf("database: user=%s password=%s host=%s\n", db.User, db.Password, db.Host)
+	}
 }
