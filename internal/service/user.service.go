06ff3e4f8ffa8efb8c99307e33b402f3e9ba60d9
Chung Nguyen <zrmedia9@gmail.com>
2024-09-20 21:48:13 +0700

    refactor: Comment out unused code in user.service.go

--- internal/service/user.service.go
+++ internal/service/user.service.go
@@ -1,17 +1,24 @@
 package service
 
-import "goecommerce/internal/repo"
+// import "goecommerce/internal/repo"
 
-type UserService struct {
-	userRepo *repo.UserRepo
+// type UserService struct {
+// 	userRepo *repo.UserRepo
+// }
+
+// func NewUserService() *UserService {
+// 	return &UserService{
+// 		userRepo: repo.NewUserRepo(),
+// 	}
+// }
+
+// func (us *UserService) GetUserInfo() string {
+// 	return us.userRepo.GetInfoUser()
+// }
+
+// Interface
+type IUserService interface {
+	Register(email string, purpose string) int
 }
 
-func NewUserService() *UserService {
-	return &UserService{
-		userRepo: repo.NewUserRepo(),
-	}
-}
-
-func (us *UserService) GetUserInfo() string {
-	return us.userRepo.GetInfoUser()
-}
+// UserService
